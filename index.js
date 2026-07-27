13f065b6a26d461af00db0bca07a22fa59b8b870
sgolbek <golbeksara@gmail.com>
2026-07-27 15:01:19 -0700

    modernized javascript variables

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
-$(document).ready(function(){
-  var currentQuestion;
-  var interval;
-  var timeLeft = 10;
-  var score = 0;
-  var highScore = 0;
+$(document).ready(function() {
+  let currentQuestion;
+  let interval;
+  let timeLeft = 10;
+  let score = 0;
+  let highScore = 0;
 
-  var updateTimeLeft = function (amount) {
+  const updateTimeLeft = function (amount) {
     timeLeft += amount;
     $('#time-left').text(timeLeft);
   };
 
-  var updateScore = function (amount) {
+  const updateScore = function (amount) {
     score += amount;
     $('#score').text(score);
   };
 
-  var updateHighScore = function () {
+  const updateHighScore = function () {
     highScore = score;
     $('#highScore').text(highScore);
   }
 
  
 
-  var startGame = function () {
+  const startGame = function () {
     if (!interval) {
       if (timeLeft === 0) {
         updateTimeLeft(10);
         updateScore(-score);
       }
+
       interval = setInterval(function () {
         updateTimeLeft(-1);
+
         if (timeLeft === 0) {
           clearInterval(interval);
           interval = undefined;
@@ -38,14 +40,14 @@ $(document).ready(function(){
     }
   };
 
-  var randomNumberGenerator = function (size) {
+  const randomNumberGenerator = function (size) {
     return Math.ceil(Math.random() * size);
   };
 
-  var questionGenerator = function () {
-    var question = {};
-    var num1 = randomNumberGenerator(10);
-    var num2 = randomNumberGenerator($(range).val());
+  const questionGenerator = function () {
+    const question = {};
+    const num1 = randomNumberGenerator(10);
+    const num2 = randomNumberGenerator($(range).val());
 
     question.answer = num1 + num2;
     question.equation = String(num1) + " + " + String(num2);
@@ -53,17 +55,18 @@ $(document).ready(function(){
     return question;
   };
 
-  var renderNewQuestion = function () {
+  const renderNewQuestion = function () {
     currentQuestion = questionGenerator();
     $('#equation').text(currentQuestion.equation);
   };
 
-  var checkAnswer = function (userInput, answer) {
+  const checkAnswer = function (userInput, answer) {
     if (userInput === answer) {
       renderNewQuestion();
       $('#user-input').val('');
-      updateTimeLeft(+1);
-      updateScore(+1);
+      updateTimeLeft(1);
+      updateScore(1);
+
       if (score > highScore) {
         updateHighScore(score);
       }
