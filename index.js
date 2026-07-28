c769383eaa2ce1308b7ad3fc7024d0003344cfb1
sgolbek <golbeksara@gmail.com>
2026-07-27 17:13:10 -0700

    Refactor game logic and input handling

--- index.js
+++ index.js
@@ -5,36 +5,46 @@ $(document).ready(function() {
   let score = 0;
   let highScore = Number(localStorage.getItem("highScore")) || 0;
 
-  const updateTimeLeft = function (amount) {
+  const $timeLeft = $("#time-left");
+  const $score = $("#score");
+  const $highScore = $("#highScore");
+  const $finalScore = $("#final-score");
+  const $gameOver = $("#game-over");
+  const $userInput = $("#user-input");
+  const $restartButton = $("#restart-button");
+  const $range = $("#range");
+  const $rangeValue = $("#rangeValue");
+  const $equation = $("#equation");
+
+  const updateTimeLeft = (amount) => {
     timeLeft += amount;
-    $('#time-left').text(timeLeft);
+    $timeLeft.text(timeLeft);
   };
 
-  const updateScore = function (amount) {
+  const updateScore = (amount) => {
     score += amount;
-    $('#score').text(score);
+    $score.text(score);
   };
 
-  const updateHighScore = function () {
+  const updateHighScore = () => {
     highScore = score;
     localStorage.setItem("highScore", String(highScore));
-    $("#highScore").text(highScore);
+    $highScore.text(highScore);
   };
 
-  const endGame = function () {
+  const endGame = () => {
     clearInterval(interval);
     interval = undefined;
 
-    $("#final-score").text(score);
-    $("#game-over").prop("hidden", false);
-    $("#user-input").prop("disabled", true);
-
-    $("#restart-button")[0].focus();
+    $finalScore.text(score);
+    $gameOver.prop("hidden", false);
+    $userInput.prop("disabled", true);
+    $restartButton[0].focus();
   };
 
-  const startGame = function () {
+  const startGame = () => {
     if (!interval && timeLeft > 0) {
-      interval = setInterval(function () {
+      interval = setInterval(() => {
         updateTimeLeft(-1);
 
         if (timeLeft === 0) {
@@ -44,67 +54,76 @@ $(document).ready(function() {
     }
   };
 
-  const restartGame = function () {
+  const restartGame = () => {
+    interval = undefined;
     timeLeft = 10;
     score = 0;
 
-    $("#time-left").text(timeLeft);
-    $("#score").text(score);
-    $("#user-input").val("").prop("disabled", false);
-    $("#game-over").prop("hidden", true);
+    $timeLeft.text(timeLeft);
+    $score.text(score);
+    $userInput.val("").prop("disabled", false);
+    $gameOver.prop("hidden", true);
 
     renderNewQuestion();
-    $("#user-input")[0].focus();
+    $userInput[0].focus();
   };
 
-  const randomNumberGenerator = function (maximum) {
+  const randomNumberGenerator = (maximum) => {
     return Math.floor(Math.random() * (maximum + 1));
   };
 
-  const questionGenerator = function () {
-    const question = {};
-    const selectedRange = Number($('#range').val());
+  const questionGenerator = () => {
+    const selectedRange = Number($range.val());
     const num1 = randomNumberGenerator(selectedRange);
     const num2 = randomNumberGenerator(selectedRange);
 
-    question.answer = num1 + num2;
-    question.equation = `${num1} + ${num2}`;
-
-    return question;
+    return {
+      answer: num1 + num2,
+      equation: `${num1} + ${num2}`
+    };
   };
 
-  const renderNewQuestion = function () {
+  const renderNewQuestion = () => {
     currentQuestion = questionGenerator();
-    $('#equation').text(currentQuestion.equation);
+    $equation.text(currentQuestion.equation);
   };
 
-  const checkAnswer = function (userInput, answer) {
-    if (userInput === answer) {
-      renderNewQuestion();
-      $('#user-input').val('');
-      updateTimeLeft(1);
-      updateScore(1);
+   const checkAnswer = (userInput, answer) => {
+    if (userInput !== answer) {
+      return;
+    }
 
-      if (score > highScore) {
-        updateHighScore();
-      }
+    renderNewQuestion();
+    $userInput.val("");
+    updateTimeLeft(1);
+    updateScore(1);
+
+    if (score > highScore) {
+      updateHighScore();
     }
   };
 
-  $(document).on('input', '#range', function () {
-    $('#rangeValue').text($(this).val());
-    renderNewQuestion();
-  })
+  $range.on("input", function () {
+    $rangeValue.text($(this).val());
 
-  $('#user-input').on('keyup', function () {
+    if (timeLeft > 0) {
+      renderNewQuestion();
+    }
+  });
+
+  $userInput.on("input", function () {
+    const inputValue = $(this).val();
+
+    if (inputValue === "") {
+      return;
+    }
+
     startGame();
-    checkAnswer(Number($(this).val()), currentQuestion.answer);
+    checkAnswer(Number(inputValue), currentQuestion.answer);
   });
 
-  $("#restart-button").on("click", function () {
-    restartGame();
-  });
+  $restartButton.on("click", restartGame);
 
-  $("#highScore").text(highScore);
+  $highScore.text(highScore);
   renderNewQuestion();
 });
